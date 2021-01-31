b793afd44a397d6f9dc69e9e7629ef30673a9d65
JBurton26 <jakeburton1997@gmail.com>
2021-01-31 22:05:21 +0000

    Bash Script Fixed

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 cd
 echo Setting Up PATH Variables.
 echo export GOPATH="/shared/go" >> .profile
-echo export GOBIN=$(go env GOPATH)/bin >> .profile
-echo export PATH=$(go env GOPATH)/bin:$PATH >> .profile
+echo export GOBIN="/shared/go/bin" >> .profile
+echo export PATH="/shared/go/bin:$PATH" >> .profile
 echo $(whoami), you will need to restart the client for changes to take effect.
 echo When you have logged back in, check to see if the GOPATH is set to "/shared/go" by using the command: "go env GOPATH"
