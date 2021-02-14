31ecafca7750be9a62595266fdcfacc91200f073
Jake Burton <40278490@live.napier.ac.uk>
2021-02-14 19:58:34 +0000

    Test script

--- test_alg.go
+++ test_alg.go
@@ -1,9 +1,27 @@
 package main
+
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.println("Hello_World")
-	
+	csvfile, err := os.Open("data/testing.csv")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	r := csv.NewReader(csvfile)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf(record[0])
+	}
 }
