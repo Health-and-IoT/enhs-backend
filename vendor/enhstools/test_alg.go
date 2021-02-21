78383385013bc53a36c73d337f14091164bbee04
Jake Burton <40278490@live.napier.ac.uk>
2021-02-21 16:43:14 +0000

    Test_alg updated - to be tested

--- vendor/enhstools/test_alg.go
+++ vendor/enhstools/test_alg.go
@@ -10,16 +10,20 @@ type Symptom struct {
 	Name string `json:"name"`
 }
 
+type Prognosis struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	SympMatch int    `json:"sympCount"`
+}
+
 // ListProgs Prints list of prognosises
 func ListProgs(recs [][]string) []string {
 	allProgs := make([]string, 0)
 	x := len(recs[1]) - 1
 	for row := range recs {
-		//records[row][column]
-		//fmt.Printf(recs[row][x] + ", ")
+
 		allProgs = append(allProgs, recs[row][x])
 	}
-	//fmt.Println()
 	return allProgs
 }
 
@@ -28,15 +32,12 @@ func ListAllSimps(recs [][]string) []byte {
 	allSimps := make([]Symptom, 0)
 	var simp Symptom
 	for col := range recs[0] {
-		//records[row][column]
-		//fmt.Printf(recs[0][col] + ", ")
 		simp.ID = col
 		simp.Name = recs[0][col]
 		allSimps = append(allSimps, simp)
 	}
 	allSimps = allSimps[:len(allSimps)-1]
 	allSimpsJSON, _ := json.Marshal(allSimps)
-	//fmt.Println()
 	return allSimpsJSON
 }
 
@@ -44,13 +45,11 @@ func ListAllSimps(recs [][]string) []byte {
 func listSimps(recs [][]string, symptom string) []string {
 	x := len(recs[1]) - 1
 	progs := make([]string, 0)
-	//fmt.Printf(symptom)
 	row := 0
 	sympIndex := -1
 	for row < x {
 		if symptom == recs[0][row] {
 			sympIndex = row
-			//fmt.Println(symptom, sympIndex)
 			break
 		}
 		row++
@@ -63,48 +62,38 @@ func listSimps(recs [][]string, symptom string) []string {
 			progs = append(progs, recs[row][x])
 		}
 	}
-	//fmt.Println(progs)
 	return progs
 }
 
 // ListSimpsMult Returns a sorted map of prognoses
-func ListSimpsMult(recs [][]string, symptoms []string) map[string]int {
+func ListSimpsMult(recs [][]string, symptoms []string) []byte {
 	progSet := make([]string, 0)
-	finProgs := make(map[string]int)
+	finProgsMap := make(map[string]int)
+	finProgsProg := make([]Prognosis, 0)
+	var progObj Prognosis
 	for _, symptom := range symptoms {
 		tempProgs := listSimps(recs, symptom)
 		progSet = append(progSet, tempProgs...)
 	}
 	for _, index := range progSet {
-		finProgs[index] = finProgs[index] + 1
+		finProgsMap[index] = finProgsMap[index] + 1
 	}
 	//fmt.Println(finProgs)
-	keys := make([]string, 0, len(finProgs))
-	for k := range finProgs {
+	keys := make([]string, 0, len(finProgsMap))
+	for k := range finProgsMap {
 		keys = append(keys, k)
 	}
 	sort.Slice(keys, func(i, j int) bool {
-		return finProgs[keys[i]] > finProgs[keys[j]]
+		return finProgsMap[keys[i]] > finProgsMap[keys[j]]
 	})
-	//for _, key := range keys {
-	//	fmt.Printf("%-7v %v\n", key, finProgs[key])
-	//}
-	return finProgs
+	index := 0
+	for prog, count := range finProgsMap {
+		progObj.ID = index
+		progObj.Name = prog
+		progObj.SympMatch = count
+		finProgsProg = append(finProgsProg, progObj)
+		index++
+	}
+	finProgsJSON, _ := json.Marshal(finProgsProg)
+	return finProgsJSON
 }
-
-//func main() {
-//	csvfile, err := os.Open("data/testing.csv")
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-//	r := csv.NewReader(csvfile)
-//	records, err := r.ReadAll()
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-//	defer csvfile.Close()
-//	str := listProgs(records)
-//	//str := listAllSimps(records)
-//	//str := listSimpsMult(records, []string{"itching", "skin_rash"})
-//	fmt.Println(str)
-//}
