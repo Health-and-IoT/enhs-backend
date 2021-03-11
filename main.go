9449ffea9f0e2108b03fa80697aabca677095d6d
Ryan <40405884@live.napier.ac.uk>
2021-03-11 13:20:58 +0000

    heap, to change

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"container/list"
 	"context"
 	"encoding/csv"
@@ -664,12 +665,53 @@ func checkForNewForm() {
 	}
 }
 
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
 //func Main - this is where it all starts
 func main() {
 	//QRCode
 	//err := qrcode.WriteFile("hi", qrcode.Medium, 256, "qr.png")
 	//Inital setup
 	//Start new queue
+	nums := []int{3, 2, 20, 5, 3, 1, 2, 5, 6, 9, 10, 4}
+
+	// initialize the heap data structure
+	h := &IntHeap{}
+
+	// add all the values to heap, O(n log n)
+	for _, val := range nums { // O(n)
+		heap.Push(h, val) // O(log n)
+	}
+
+	// print all the values from the heap
+	// which should be in ascending order
+	for i := 0; i < len(nums); i++ {
+		fmt.Printf("%d,", heap.Pop(h).(int))
+	}
 	queue := list.New()
 	color.Green("Backend server started! ✔️")
 	//Check for logfile - if none, create one.
